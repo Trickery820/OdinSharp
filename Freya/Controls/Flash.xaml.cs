10438e9a2e902ec446d4da8c0e31ad487717b042
Flx23xs <Flx23xs@outlook.com>
2022-02-04 01:23:15 +0800

    :memo: first.md

---
+++ Freya/Controls/Flash.xaml.cs
@@ -0,0 +1,400 @@
+﻿using Freya.Util;
+using Microsoft.Win32;
+using SharpOdinClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Freya.Util.Util;
+using static SharpOdinClient.util.utils;
+
+namespace Freya.Controls
+{
+    /// <summary>
+    /// Interaction logic for Flash.xaml
+    /// </summary>
+    public partial class Flash : UserControl
+    {
+        public FlashField BLPackage = new FlashField("BL (bootloader) file package [tar,md5]");
+        public FlashField APPackage = new FlashField("AP (PDA) file package [tar,md5]");
+        public FlashField CPPackage = new FlashField("CP (Modem) file package [tar,md5]");
+        public FlashField CSCPackage = new FlashField("CSC file package [tar,md5]");
+        public Odin Odin = new Odin();
+
+        public event ProgressChangedDelegate ProgressChanged;
+        public event LogDelegate Log;
+        public event IsRunningProcessDelegate IsRunning;
+
+        public Flash()
+        {
+            InitializeComponent();
+
+            Features.Children.Add(BLPackage);
+            Features.Children.Add(APPackage);
+            Features.Children.Add(CPPackage);
+            Features.Children.Add(CSCPackage);
+            BLPackage.PitDetect += BLPackage_PitDetect;
+            APPackage.PitDetect += BLPackage_PitDetect;
+            CPPackage.PitDetect += BLPackage_PitDetect;
+            CSCPackage.PitDetect += BLPackage_PitDetect;
+
+            RepartitionCheckBx.IsEnabled = false;
+            BtnClearPit.Visibility = Visibility.Collapsed;
+            Odin.Log += Odin_Log; ;
+            Odin.ProgressChanged += Odin_ProgressChanged; 
+
+        }
+
+
+        public void ControlsManage(bool IsEnable)
+        {
+            BLPackage.IsEnabled = !IsEnable;
+            APPackage.IsEnabled = !IsEnable;
+            CPPackage.IsEnabled = !IsEnable;
+            CSCPackage.IsEnabled = !IsEnable;
+            AutoBoot.IsEnabled = !IsEnable;
+            BootUpdate.IsEnabled = !IsEnable;
+            EfsClear.IsEnabled = !IsEnable;
+            TxtBxPit.IsEnabled = !IsEnable;
+            BtnClearPit.IsEnabled = !IsEnable;
+            BtnChoosePit.IsEnabled = !IsEnable;
+            BtnFlash.IsEnabled = !IsEnable;
+            BtnReadPit.IsEnabled = !IsEnable;
+        }
+
+        private void Odin_Log(string Text, MsgType Color, bool IsError = false)
+        {
+            Log?.Invoke(Text, Color, IsError);
+        }
+
+        private void Odin_ProgressChanged(string filename, long max, long value, long WritenSize)
+        {
+            ProgressChanged?.Invoke(filename, max, value, WritenSize);
+        }
+        private void BtnChoosePit_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                DefaultExt = ".tar",
+                Filter = "Csc Or Pit file|*.tar;*.md5;*.pit"
+            };
+            bool? result = dlg.ShowDialog();
+            if (result == true)
+            {
+                TxtBxPit.Clear();
+                string filename = dlg.FileName;
+                var ext = System.IO.Path.GetExtension(filename);
+                if (ext == ".pit")
+                {
+                    BLPackage_PitDetect(System.IO.Path.GetFileName(filename), filename);
+                }
+                else
+                {
+                    var item = this.Odin.tar.TarInformation(filename);
+                    if (item.Count > 0)
+                    {
+                        foreach (var Tiem in item)
+                        {
+                            var Extension = System.IO.Path.GetExtension(Tiem.Filename);
+                            var file = new FileFlash
+                            {
+                                Enable = true,
+                                FileName = Tiem.Filename,
+                                FilePath = filename
+                            };
+
+                            if (Extension == ".pit")
+                            {
+                                BLPackage_PitDetect(Tiem.Filename, filename);
+                                return;
+                            }
+                        }
+                    }
+
+                }
+
+
+            }
+        }
+        private void BtnClearPit_Click(object sender, RoutedEventArgs e)
+        {
+            TxtBxPit.Clear();
+            RepartitionCheckBx.IsChecked = false;
+        }
+        private async void BLPackage_PitDetect(string pitname, string path)
+        {
+            RepartitionCheckBx.IsChecked = false;
+            var extension = System.IO.Path.GetExtension(path);
+            if (extension.ToLower() == ".pit")
+            {
+                if (Odin.PitTool.UNPACK_PIT(File.ReadAllBytes(path)))
+                {
+                    TxtBxPit.Text = path;
+                    RepartitionCheckBx.IsChecked = true;
+                }
+                else
+                {
+                    Log?.Invoke($"File Curreped : {pitname}", MsgType.Message , true);
+                    return;
+                }
+            }
+            else
+            {
+                var pit = await Odin.tar.ExtractFileFromTar(path, pitname);
+                if (pit.Length == 0 || !Odin.PitTool.UNPACK_PIT(pit))
+                {
+                    Log?.Invoke($"File Curreped : {pitname}", MsgType.Message, true);
+                    return;
+                }
+                else
+                {
+                    TxtBxPit.Text = path;
+                    RepartitionCheckBx.IsChecked = true;
+                }
+            }
+        }
+
+        public async Task DoFlash(long Size, List<FileFlash> ListFlash)
+        {
+            var list = new List<SharpOdinClient.structs.FileFlash>();
+            foreach(var i in ListFlash)
+            {
+                list.Add(new SharpOdinClient.structs.FileFlash
+                {
+                    Enable = i.Enable,
+                    FileName = i.FileName,
+                    FilePath = i.FilePath,
+                    RawSize = i.RawSize
+                });
+
+            }
+            if (!await Odin.FindAndSetDownloadMode())
+            {
+                return;
+            }
+            await Odin.PrintInfo();
+            Log?.Invoke("Checking Download Mode : ", MsgType.Message);
+            if (await Odin.IsOdin())
+            {
+                Log?.Invoke("ODIN", MsgType.Result);
+                Log?.Invoke($"Initializing Device : ", MsgType.Message);
+                if (await Odin.LOKE_Initialize(Size))
+                {
+                    Log?.Invoke("Initialized", MsgType.Result);
+                    if (!string.IsNullOrEmpty(TxtBxPit.Text) && RepartitionCheckBx.IsChecked == true )
+                    {
+                        Log?.Invoke("Repartition Device : ", MsgType.Message);
+                        var Repartition = await Odin.Write_Pit(TxtBxPit.Text);
+                        if (Repartition.status)
+                        {
+                            Log?.Invoke("Ok", MsgType.Result);
+                        }
+                        else
+                        {
+                            Log?.Invoke(Repartition.error, MsgType.Result , true);
+                            return;
+                        }
+                    }
+
+                    Log?.Invoke("Reading Pit from device : ", MsgType.Message);
+                    var GetPit = await Odin.Read_Pit();
+                    if (GetPit.Result)
+                    {
+                        Log?.Invoke("Ok", MsgType.Result );
+                        var EfsClearInt = 0;
+                        var BootUpdateInt = 0;
+                        if (EfsClear.IsChecked == true)
+                        {
+                            EfsClearInt++;
+                        }
+                        if (BootUpdate.IsChecked == true)
+                        {
+                            BootUpdateInt++;
+                        }
+                        if (await Odin.FlashFirmware(list, GetPit.Pit, EfsClearInt, BootUpdateInt, true))
+                        {
+                            if (AutoBoot.IsChecked == true)
+                            {
+                                Log?.Invoke("Rebooting Device To Normal Mode : ", MsgType.Message);
+                                if (await Odin.PDAToNormal())
+                                {
+                                    Log?.Invoke("Ok", MsgType.Result );
+                                }
+                                else
+                                {
+                                    Log?.Invoke("Failed", MsgType.Result , true);
+                                }
+                            }
+                            else
+                            {
+                                Log?.Invoke("Auto Reboot Disabled Try Manual", MsgType.Message);
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        Log?.Invoke("Failed", MsgType.Result , true);
+                    }
+                }
+                else
+                {
+                    Log?.Invoke("Failed", MsgType.Result , true);
+                }
+            }
+            else
+            {
+                Log?.Invoke("Failed", MsgType.Result , true);
+            }
+        }
+
+        private async void BtnFlash_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+                IsRunning?.Invoke(true, "Flash");
+                var ListFlash = new List<FileFlash>();
+                ListFlash.AddRange(BLPackage.Files);
+                ListFlash.AddRange(APPackage.Files);
+                ListFlash.AddRange(CPPackage.Files);
+                ListFlash.AddRange(CSCPackage.Files);
+                if (ListFlash.Count > 0)
+                {
+                    Log?.Invoke("Calculated Size : ", MsgType.Message);
+                    var Size = 0L;
+                    foreach (var item in ListFlash)
+                    {
+                        Size += item.RawSize;
+                    }
+                    if (Size > 0)
+                    {
+                        Log?.Invoke(Util.Util.GetBytesReadable(Size),MsgType.Result );
+                        await DoFlash(Size, ListFlash);
+                    }
+                    else
+                    {
+                        Log?.Invoke("Failed",MsgType.Result , true);
+                    }
+                }
+                else if (!string.IsNullOrEmpty(TxtBxPit.Text) && RepartitionCheckBx.IsChecked == true)
+                {
+                    await DoFlash(0, ListFlash);
+                }
+                else
+                {
+                    Log?.Invoke("Please Select Firmware Package and try again", MsgType.Message);
+                }
+
+            }
+            catch (Exception ee)
+            {
+                Log?.Invoke($"System Error : ", MsgType.Message);
+                Log?.Invoke(ee.Message, MsgType.Result , true);
+
+            }
+            finally
+            {
+                IsRunning?.Invoke(false, "Flash");
+            }
+        }
+
+        private void TxtBxPit_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtBxPit.Text))
+            {
+                RepartitionCheckBx.IsEnabled = false;
+                BtnClearPit.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                RepartitionCheckBx.IsEnabled = true;
+                BtnClearPit.Visibility = Visibility.Visible;
+            }
+
+        }
+
+        private async void BtnReadPit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!await Odin.FindAndSetDownloadMode())
+                {
+                    return;
+                }
+                IsRunning?.Invoke(true, "ReadPit");
+                await Odin.PrintInfo();
+                Log?.Invoke("Checking Download Mode : ", MsgType.Message);
+                if (await Odin.IsOdin())
+                {
+                    Log?.Invoke("ODIN",MsgType.Result );
+                    Log?.Invoke("Initializing Device : ", MsgType.Message);
+                    if (await Odin.LOKE_Initialize(0))
+                    {
+                        Log?.Invoke("Initialized",MsgType.Result );
+                        Log?.Invoke("Reading Pit : ", MsgType.Message);
+                        var GetPit = await Odin.Read_Pit();
+                        if (GetPit.Result)
+                        {
+                            Log?.Invoke("Ok",MsgType.Result );
+                            Log?.Invoke($"SavedPath : ", MsgType.Message);
+                            var fpath = $"{Util.Util.MyPath}\\backup\\samsung\\pit\\{DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")}.pit";
+                            Util.Util.CreatFolder(System.IO.Path.GetDirectoryName(fpath));
+                            System.IO.File.WriteAllBytes(fpath, GetPit.data);
+                            Log?.Invoke(fpath,MsgType.Result );
+                            if (AutoBoot.IsChecked == true)
+                            {
+                                Log?.Invoke("Rebooting Device To Normal Mode : ", MsgType.Message);
+                                if (await Odin.PDAToNormal())
+                                {
+                                    Log?.Invoke("Ok",MsgType.Result );
+                                }
+                                else
+                                {
+                                    Log?.Invoke("Failed", MsgType.Result , true);
+                                }
+                            }
+                            else
+                            {
+                                Log?.Invoke("Auto Reboot Disabled Try Manual", MsgType.Message);
+                            }
+                        }
+                        else
+                        {
+                            Log?.Invoke("Failed", MsgType.Result , true);
+                        }
+                    }
+                    else
+                    {
+                        Log?.Invoke("Failed", MsgType.Result , true);
+                    }
+                }
+                else
+                {
+                    Log?.Invoke("Failed", MsgType.Result , true);
+                }
+            }
+            catch (Exception ee)
+            {
+                Log?.Invoke($"System Error : ", MsgType.Message);
+                Log?.Invoke(ee.Message, MsgType.Result , true);
+
+            }
+            finally
+            {
+                IsRunning?.Invoke(false, "ReadPit");
+            }
+        }
+    }
+}
---
+++ Freya/window/Main.xaml.cs
@@ -0,0 +1,190 @@
+﻿using Freya.Class;
+using Freya.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Freya.window
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+
+        public Flash Flash;
+        public Main()
+        {
+            InitializeComponent();
+            Flash = new Flash();
+            Flash.Log += Flash_Log;
+            Flash.ProgressChanged += Flash_ProgressChanged;
+            Flash.IsRunning += Flash_IsRunning;
+            FrmMain.Navigate(Flash);
+        }
+
+        private void Flash_IsRunning(bool IsRunning, string process)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Flash.ControlsManage(IsRunning);
+                BtnStop.IsEnabled = IsRunning;
+            });
+        }
+
+        private void Flash_ProgressChanged(string filename, long max, long value, long WritenSize)
+        {
+            Application.Current.Dispatcher.Invoke(() => {
+                ProgBar.Maximum = max;
+                ProgBar.Value = value;
+                Events.Content = $"{filename} | {WritenSize:###,###,###}";
+            });
+        }
+
+        private void Flash_Log(string Text, SharpOdinClient.util.utils.MsgType Color, bool IsError = false)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (string.IsNullOrEmpty(Text))
+                {
+                    return;
+                }
+                TextRange rangeOfText1 = new TextRange(RichLog.Document.ContentEnd, RichLog.Document.ContentEnd);
+                if (Color == SharpOdinClient.util.utils.MsgType.Message)
+                {
+                    Text = $"\n{Text}";
+                }
+                rangeOfText1.ApplyPropertyValue(TextBlock.TextAlignmentProperty, TextAlignment.Left);
+                RichLog.FlowDirection = FlowDirection.LeftToRight;
+                rangeOfText1.Text = Text;
+                if(IsError)
+                {
+                    rangeOfText1.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.YellowGreen);
+                }else if(Color == SharpOdinClient.util.utils.MsgType.Result)
+                {
+                    rangeOfText1.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Cyan);
+                }
+                else
+                {
+                    rangeOfText1.ApplyPropertyValue(TextElement.ForegroundProperty, (SolidColorBrush)new BrushConverter().ConvertFrom("#59b369"));
+                }
+                switch (Color)
+                {
+                    case SharpOdinClient.util.utils.MsgType.Result:
+                        {
+                            rangeOfText1.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+                            break;
+                        }
+                    case SharpOdinClient.util.utils.MsgType.Message:
+                        {
+                            rangeOfText1.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+                            break;
+                        }
+                }
+                RichLog.ScrollToEnd();
+            });
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Application.Current.Shutdown();
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void RichLog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (RichLog.Document.Blocks.Count == 0)
+            {
+                BtnClearRich.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BtnClearRich.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BtnMinMaximizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                MxIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                MxIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowRestore;
+            }
+
+        }
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void BtnMinMaxWindow_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+
+        }
+       
+
+        private void BtnClearRich_Click(object sender, RoutedEventArgs e)
+        {
+            RichLog.Document.Blocks.Clear();
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            Flash.Odin.StopOperations();
+        }
+
+        private void lblAlephGsm_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://alephgsm.com");
+
+        }
+
+        private void lblXDA_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://forum.xda-developers.com/m/asgharso.8554421/");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Flash_Log("════════════════════════════════════════════",  SharpOdinClient.util.utils.MsgType.Message);
+            Flash_Log("Freya v" + System.Reflection.Assembly.GetEntryAssembly().GetName().Version.ToString(),  SharpOdinClient.util.utils.MsgType.Message);
+            Flash_Log("════════════════════════════════════════════",  SharpOdinClient.util.utils.MsgType.Message);
+        }
+
+        private void LblGihub_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/Alephgsm");
+        }
+
+        private void ScreenShot_Click(object sender, RoutedEventArgs e)
+        {
+            var image = ScreenCapture.CaptureActiveWindow();
+            var SavePath = $"{Util.Util.MyPath}\\backup\\Screenshot\\{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.Jpeg";
+            Util.Util.CreatFolder($"{Util.Util.MyPath}\\backup\\Screenshot");
+            image.Save(SavePath, ImageFormat.Jpeg);
+            Flash_Log("ScreenShot Saved : ", SharpOdinClient.util.utils.MsgType.Message);
+            Flash_Log(SavePath, SharpOdinClient.util.utils.MsgType.Result);
+        }
+    }
+}
